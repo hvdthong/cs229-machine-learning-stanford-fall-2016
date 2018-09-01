37ef157dc121061fe5b4910290ee99d025622958
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2018-09-01 10:00:43 +0700

    adding logistic regression

--- src/homework1/logistic_regression.py
+++ src/homework1/logistic_regression.py
@@ -2,12 +2,12 @@ import numpy as np
 import numpy.linalg as lin
 
 MAX_ITERS = 20
-EPSILON   = 1.0e-7
+EPSILON = 1.0e-7
 
 
 def h(theta, X, y):
     margins = y * X.dot(theta)
-    
+
     return 1 / (1 + np.exp(-margins))
 
 
@@ -15,22 +15,22 @@ def J(theta, X, y):
     probs = h(theta, X, y)
     mm = probs.size
 
-    return (-1 / mm) * np.sum(np.log(probs))
+    return (-1 / float(mm)) * np.sum(np.log(probs))
 
 
 def gradJ(theta, X, y):
     probs = h(theta, X, -y) * y
     mm = probs.size
 
-    return (-1 / mm) * X.T.dot(probs)
+    return (-1 / float(mm)) * X.T.dot(probs)
 
 
 def hessJ(theta, X, y):
-    d     = h(theta, X, y)
+    d = h(theta, X, y)
     probs = d * (1 - d)
-    mm    = probs.size
+    mm = probs.size
     prob_vec = np.diag(probs)
-    hessJ = (1 / mm) * (X.T.dot(prob_vec).dot(X))
+    hessJ = (1 / float(mm)) * (X.T.dot(prob_vec).dot(X))
 
     return hessJ
 
@@ -45,9 +45,9 @@ def logistic_regression(X, y, epsilon, max_iters):
 
     for i in range(0, max_iters):
         cost[i] = J(theta, X, y)
-        grad    = gradJ(theta, X, y)
-        H       = hessJ(theta, X, y)
-        Hinv    = lin.inv(H)
-        theta   = theta - Hinv.dot(grad)
+        grad = gradJ(theta, X, y)
+        H = hessJ(theta, X, y)
+        Hinv = lin.inv(H)
+        theta = theta - Hinv.dot(grad)
 
     return (theta, cost)
