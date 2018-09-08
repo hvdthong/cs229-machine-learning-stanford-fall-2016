b97e8986864efc34441fe90f73540f649f7e506d
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2018-09-08 21:48:56 +0800

    adding homework1

--- src/homework1/homework1.py
+++ src/homework1/homework1.py
@@ -2,7 +2,6 @@ import logistic_regression as lr
 import numpy as np
 import matplotlib as plt
 
-
 fileX = 'logistic_x.txt'
 fileY = 'logistic_y.txt'
 
@@ -10,12 +9,12 @@ fileY = 'logistic_y.txt'
 def load_data(fileX, fileY):
     X = np.loadtxt(fileX)
     y = np.loadtxt(fileY)
-    ones = np.ones((99,1))
+    ones = np.ones((99, 1))
     Xsplit = np.split(X, indices_or_sections=[1], axis=1)
     # Pack the intercept coordinates into X so we can calculate the 
     # intercept for the logistic regression.
     X = np.concatenate([ones, Xsplit[0], Xsplit[1]], axis=1)
-    
+
     return (X, y)
 
 
@@ -30,3 +29,8 @@ def main():
 
     print('theta = {}'.format(theta))
     print('cost = {}'.format(cost))
+
+
+if __name__ == '__main__':
+    regression()
+    main()
